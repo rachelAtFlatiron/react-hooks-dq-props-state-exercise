4acd4353c36f4ac35c54723c9dbf84df8879cb09
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-04-29 11:21:31 -0400

    first commit

--- src/components/BabyHog.js
+++ src/components/BabyHog.js
@@ -1,32 +1,49 @@
-import React from "react";
+import React, {useState}from "react";
 import normalBaby from "../assets/unadulterated-hoglette.png";
 import SunBaby from "../assets/sun-eyes.png";
 import BlueBaby from "../assets/blue-eyes.png";
 import GlowingBaby from "../assets/glowing-eyes.png";
 
+
 // is there a way we could associate eye color string values with images?
 // perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
 
-function BabyHog(props) {
+function BabyHog({eyeColor, name, hobby}) {
+  //create state for weight and set inital value to 0
+  let [weight, setWeight] = useState(50);
+
+  //create function to use setter function to increase or decrease depending on which button was pressed
   function handleChangeWeight(e) {
+    if(e.target.name === "+"){
+      //increase by 50lbs
+      setWeight(weight +=50)
+    }else{
+      //decrease by 50lbs
+      setWeight(weight -=50)
+    }
     // how can we reuse this for both buttons?
     // perhaps something with e.target.name === "+"
   }
 
   return (
+    //this returns JSX of each hog baby with details of name, hobby, eye color with imported props from parent
+    //component
     <li className="hogbabies">
-      <h1>Name</h1>
-      <h3>Weight:</h3>
-      <h3>Hobby:</h3>
-      <h4>Eye Color:</h4>
+      <h1>Name: {name}</h1>
+      {/*Weight is state and changes within this component by handleChangeWeight funciton  */}
+      <h3>Weight: {weight}lbs</h3>
+      <h3>Hobby: {hobby}</h3>
+      <h4>Eye Color: {eyeColor}</h4>
 
-      <button name="+">Increase Weight</button>
-      <button name="-">Decrease Weight</button>
+      {/* buttons with onclick event to increase and decrease the weight of hog baby */}
+      <button name="+" onClick={handleChangeWeight}>Increase Weight</button>
+      <button name="-" onClick={handleChangeWeight}>Decrease Weight</button>
 
+      {/* this is where we change the height of the hog baby image by using weight state */}
       <div className="hb-wrap">
         <img
           src={normalBaby}
-          style={{ height: "200px" }}
+          style={{ height: `${weight}px` }}
           alt="MasterBlasterJrJr"
         />
       </div>
--- src/components/HogBoss.js
+++ src/components/HogBoss.js
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
 import Boss from "../assets/boss-hog.png";
 import BabyHog from "./BabyHog";
-// import offspring from "../data.js"
+import offspring from "../data.js"
 
 function HogBoss() {
+  //create state with eyeColor that will be passed down to child
   const [eyeColor, setEyeColor] = useState("blue");
-
+  
+  //function to handle eyecolor change, uses setter function to change eyecolor onChange event
   function handleChangeEyeColor(e) {
     setEyeColor(e.target.value);
   }
-
+  //create variable that iterates through offspring data and render baby hog with passed down props
+  let renderBabyHogs = offspring.map((hog)=> {
+   return ( <BabyHog key={hog.id} name={hog.name} hobby={hog.hobby} eyeColor={eyeColor}/>)
+  })
+  console.log(renderBabyHogs)
+  
   return (
+    //JSX for list of eye colors with onChange event on each
     <div>
       <input
         type="radio"
@@ -33,6 +41,7 @@ function HogBoss() {
         onChange={handleChangeEyeColor}
       />
       Glowing<br></br>
+      {/* JSX for HogBoss info */}
       <h2>Name: Blaster Boss</h2>
       <h3>Weight: 2.54 Tons</h3>
       <h3>Eye Color: {eyeColor}</h3>
@@ -40,9 +49,7 @@ function HogBoss() {
         <img id="boss-blaster" src={Boss} alt="" />
       </div>
       <ul className="hoglist">
-        <BabyHog />
-        <BabyHog />
-        <BabyHog />
+       {renderBabyHogs}
       </ul>
     </div>
   );
